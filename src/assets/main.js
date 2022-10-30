3f673fa6e0df3f15f7ac72d81d4a9b15bf3ea7ce
Alejandro Mazas de Lizana <alejandro.mazasdelizana@gmail.com>
2022-10-30 19:40:46 +0100

    landing page fixed

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,7 +10,7 @@ const options = {
     }
 };
 
-const fetchData = async (urlapi) => {
+async function fetchData(urlapi) {
     const response = await fetch(urlapi, options)
     const data = await response.json()
     return data
@@ -18,13 +18,13 @@ const fetchData = async (urlapi) => {
 
 (async () => {
     try {
-        const videos = await fetchData(urlapi)
+        const videos = await fetchData(API)
         let view = `
         ${videos.items.map(video => `
         <div class="group relative">
                     <div
                         class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-                        <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
+                        <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
                     </div>
                     <div class="mt-4 flex justify-between">
                         <h3 class="text-sm text-gray-700">
